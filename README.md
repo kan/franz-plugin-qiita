4d991ee72d3d7e1c42507b89062dcfe34861eed3
Kan Fushihara <kan.fushihara@gmail.com>
2016-09-15 16:37:00 +0900

    initial import

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # franz-plugin-qiita
+
+[Franz](http://meetfranz.com/) plugin for [Qiita](http://qiita.com/) and [Qiita:Team](https://teams.qiita.com/).
---
+++ qiita/webview.js
@@ -0,0 +1,20 @@
+module.exports = (Franz, options) => {
+    function getMessages() {
+        $('.item-box-title h1 a').each((i, e) => {
+            $(e).attr('target', '_blank');
+        });
+
+        var unread = 0;
+        const flush = $('div.flush-button.active')[0];
+        if (flush) {
+            const res = flush.textContent.match(/(\d+) 件の新しいアクティビティ/);
+            if(res) {
+                unread = res[1];
+            }
+        }
+        const notify = parseInt($('div.globalNotifications_count')[0].textContent)
+        Franz.setBadge(notify, unread);
+    }
+
+    Franz.loop(getMessages);
+}
